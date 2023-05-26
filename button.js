20397d6b735ffc9a2c0b0c28e502455ffb3581b7
Slonew <132140953+Slonwer@users.noreply.github.com>
2023-05-26 16:55:34 -0300

    Add files via upload
    Arquivos Soltos

---
+++ button.js
@@ -0,0 +1,11 @@
+ var button = onclick
+
+function hardClick(event){
+    console.log("Botão click", botaoClicado)
+
+}
+var meuBotao = document.getElementById("Meu botao")
+
+meuBotao.addEventListener("click", hardClick)
+
+    
---
+++ main.js
@@ -0,0 +1,24 @@
+function preenchaTudo(){
+    alert('Preenha todos os campos!')
+    document.write("Obrigatorio!!")
+
+}
+
+function redireciona(opcao){
+    if (opcao === 1) {
+        
+    }else if(opcao === 2) {
+
+    }else{
+
+    }
+}
+
+function sucesso() {
+    
+}
+
+function erro() {
+    document.write(prompt("Sua função não deu certo"))
+    
+}
